ec1a8dbfcf2b320ef7fe50b511f99cb17268a395
Michael <m.squillacioti16@gmail.com>
2022-11-16 15:51:22 -0500

    Updates

---
+++ precache-manifest.093c5b9faeea2b8a82d0a16b677360b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6696cccbb52c701b8443",
+    "url": "/my-portfolio/static/css/main.f632b6a3.chunk.css"
+  },
+  {
+    "revision": "6696cccbb52c701b8443",
+    "url": "/my-portfolio/static/js/main.e4abb74a.chunk.js"
+  },
+  {
+    "revision": "1d5e5cfaa815133aab44",
+    "url": "/my-portfolio/static/js/runtime~main.fa765fd7.js"
+  },
+  {
+    "revision": "2a977433ef3b9bcbb5cc",
+    "url": "/my-portfolio/static/js/2.195b2395.chunk.js"
+  },
+  {
+    "revision": "8ddd51cce2f1adc55b02",
+    "url": "/my-portfolio/static/js/3.92ff1598.chunk.js"
+  },
+  {
+    "revision": "bc745af31dcc552cf3d7ca5c5a9d3798",
+    "url": "/my-portfolio/static/media/Resume_Michael_Squillacioti.bc745af3.pdf"
+  },
+  {
+    "revision": "1128f7afc3a6061f20f17c4cddf9dd1a",
+    "url": "/my-portfolio/static/media/profilepicture.1128f7af.png"
+  },
+  {
+    "revision": "e4d1acafd72f4a654fa7c2a1b4de43cd",
+    "url": "/my-portfolio/index.html"
+  }
+];
