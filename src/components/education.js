647aa2df22700864816a6bc9cf5b1375a8d153dc
Michael Squillacioti <msquill@ncsu.edu>
2023-05-18 17:10:45 -0400

    Added NC State to education, and updated to 2023 4.1 resume. Includes work experience now.

--- src/components/education.js
+++ src/components/education.js
@@ -1,25 +1,31 @@
 const education = [
-  {
-    schoolName: "The London App Brewery",
-    img:
-      "https://external-preview.redd.it/7fARLi7jaV2QXgde4-NMdWcg2zuVAuAG63foskFzk-w.jpg?auto=webp&s=736b2671d1b3b321e75969b2f3f0ed10f61d60e0",
-    time: "June 2022 - November 2022",
-    degree: "Certificate: Full Stack Web Development Bootcamp"
-  },
-  {
-    schoolName: "The University of Alabama",
-    img:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Alabama_Crimson_Tide_logo.svg/800px-Alabama_Crimson_Tide_logo.svg.png",
-    time: "May 2019 - December 2021",
-    degree: "MBA, with focus in Optimization and Python"
-  },
-  {
-    schoolName: "The University of Alabama",
-    img:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Alabama_Crimson_Tide_logo.svg/800px-Alabama_Crimson_Tide_logo.svg.png",
-    time: "August 2016 - December 2020",
-    degree: "BS in Mechanical Engineering"
-  }
+    {
+        schoolName: "North Carolina State University",
+        img: "https://cdn.freebiesupply.com/logos/large/2x/nc-state-university-logo-png-transparent.png",
+        time: "January 2023 - Present",
+        degree: "Certificate: Computer Programming",
+    },
+    {
+        schoolName: "The London App Brewery",
+        img:
+        "https://external-preview.redd.it/7fARLi7jaV2QXgde4-NMdWcg2zuVAuAG63foskFzk-w.jpg?auto=webp&s=736b2671d1b3b321e75969b2f3f0ed10f61d60e0",
+        time: "June 2022 - November 2022",
+        degree: "Certificate: Full Stack Web Development Bootcamp"
+    },
+    {
+        schoolName: "The University of Alabama",
+        img:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Alabama_Crimson_Tide_logo.svg/800px-Alabama_Crimson_Tide_logo.svg.png",
+        time: "May 2019 - December 2021",
+        degree: "MBA, with focus in Optimization and Python"
+    },
+    {
+        schoolName: "The University of Alabama",
+        img:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Alabama_Crimson_Tide_logo.svg/800px-Alabama_Crimson_Tide_logo.svg.png",
+        time: "August 2016 - December 2020",
+        degree: "BS in Mechanical Engineering"
+    }
 ];
 
 export default education;
