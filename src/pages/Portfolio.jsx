a12e963f33693df1969e71daa98708957fb0ecd5
Michael Squillacioti <msquill@ncsu.edu>
2023-05-18 17:52:24 -0400

    minor sizing fixes

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -13,7 +13,9 @@ function Portfolio() {
         <div>
           <h1 class="title">Portfolio</h1>
         </div>
-        <h3>* Portfolio COMING SOON *</h3>
+        <h3></h3>
+        <h3>* Portfolio Coming Soon *</h3>
+        <h3></h3>
         <img src={"http://www.clker.com/cliparts/1/c/2/a/11970890841431512955Milkman666_Cone.svg.svg.hi.png"} alt={"Traffic Cone"} />
       </div>
       <DownloadResume />
